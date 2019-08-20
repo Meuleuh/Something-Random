323bfe0f3beac2647242a5839a78bdd222d7eb87
Meuleuh <32850732+Meuleuh@users.noreply.github.com>
2019-08-20 10:36:02 -0500

    Commit 3
    Started making a Matrix Screen Effect

---
+++ Something/Whatever's up on my mind/General Stuff/RandomChar.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Whatever_s_up_on_my_mind
+{
+    public partial class GeneralStuff
+    {
+        private static Random random = new Random();
+        public static string randomchar(int length)
+        {
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            return new string(Enumerable.Repeat(chars, length).Select(s => s[random.Next(s.Length)]).ToArray());
+        }
+    }
+}
