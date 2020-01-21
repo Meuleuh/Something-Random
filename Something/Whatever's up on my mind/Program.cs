6624ef0600855f036131e1c79f8a0bda78417a23
Meuleuh <32850732+Meuleuh@users.noreply.github.com>
2020-01-21 10:52:24 -0500

    Something changed

--- Something/Whatever's up on my mind/Program.cs
+++ Something/Whatever's up on my mind/Program.cs
@@ -27,20 +27,7 @@ namespace Whatever_s_up_on_my_mind
             switch (ChoixInt)
             {
                 case 1:
-                    Console.Clear();
-                    Console.CursorVisible = false;
-                    Console.SetWindowSize(Console.LargestWindowWidth, Console.LargestWindowHeight);
-                    Console.ForegroundColor = ConsoleColor.Green;
-                    for (int i = 0; i < 100; i--)
-                    {
-                        System.Threading.Thread.Sleep(10);
-                        for (int j = 0; j < Console.WindowHeight; j++)
-                        {
-                            string AChar = GeneralStuff.randomchar(Console.WindowWidth);
-                            Console.SetCursorPosition(0, j);
-                            Console.WriteLine(AChar);
-                        }
-                    }
+                    RandomStuff.MatrixCharacterRainingEffect();
                     break;
             }
         }
---
+++ Something/Whatever's up on my mind/Random Stuff/MatrixCharacterRainingEffect.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Whatever_s_up_on_my_mind
+{
+    public partial class RandomStuff
+    {
+        public static void MatrixCharacterRainingEffect()
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.Clear();
+            Console.CursorVisible = false;
+//            Console.SetWindowSize(Console.LargestWindowWidth, Console.LargestWindowHeight);
+            Console.ForegroundColor = ConsoleColor.Green;
+            for (int k = 0; k < 1; k--)
+            {
+                for (int i = 0; i < Console.WindowWidth; i++)
+                {
+                    if (random.Next(100) < 1)
+                    {
+                        Console.Write(randomchar(1));
+                    }
+                    else
+                    {
+                        Console.Write(" ");
+                    }
+                }
+                for (int j = 1; j < Console.WindowHeight; j++)
+                {
+                    for (int i = 0; i < Console.WindowWidth; i++)
+                    {
+
+                    }
+                }
+            }
+            Console.SetCursorPosition(0, 0);
+            System.Threading.Thread.Sleep(500);
+        }
+        private static Random random = new Random();
+
+        public static string randomchar(int length)
+        {
+            const string chars = "┏┓┗┛┣┫┳┻╋";
+            return new string(Enumerable.Repeat(chars, length).Select(s => s[random.Next(s.Length)]).ToArray());
+        }
+    }
+}
